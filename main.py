796901d5a257781d6126233c2966788822ddfb08
Daniel <dotonesimple@gmail.com>
2023-01-25 10:24:19 +0100

    add opengl

--- main.py
+++ main.py
@@ -1,7 +1,83 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
 from mp import MpThread
 
 thread = MpThread(1, "Thread-1", 1)
 
 thread.start()
 
-BaseContext = testingcontext.getInteractive()
+verticies = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
+
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+
+
+
+def drawText(x, y, text, font):
+    textSurface = font.render(text, True, (255, 255, 66, 255), (0, 66, 0, 255))
+    textData = pygame.image.tostring(textSurface, "RGBA", True)
+    glWindowPos2d(x, y)
+    glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
+
+
+def main():
+    pygame.init()
+    font = pygame.font.SysFont('Arial', 64)
+
+    display = (800, 600)
+    pygame.display.set_mode((0, 0), DOUBLEBUF | OPENGL | FULLSCREEN)
+    # pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0, 0.0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        Cube()
+        drawText(140, 120, str(thread.coords[0]) + ", " + str(thread.coords[1]), font)
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+main()
